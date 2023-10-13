597cb7a5c24d2b7b4a82292e7b7f5722023dc46c
Elishama-Pintosevich <127098904+Elishama-Pintosevich@users.noreply.github.com>
2023-10-13 16:55:30 +0300

    Add files via upload

--- models/tractateModel.js
+++ models/tractateModel.js
@@ -4,7 +4,8 @@ const Joi = require("joi");
 const tractateSchema = new mongoose.Schema({
   name:String,
   count:Number,
-  pages:[Number]
+  pages:[Number],
+  amud:String
 },{timestamps:true})
 
 exports.TractateModel = mongoose.model("tractates",tractateSchema);
@@ -13,7 +14,8 @@ exports.validateTractate = (_reqbody) => {
   const joiSchema = Joi.object({
     name: Joi.string().min(2).max(100).required(),
     count:Joi.number().min(2).max(500).required(),
-    pages:Joi.array().items(Joi.number()).required()
+    pages:Joi.array().items(Joi.number()).required(),
+    amud:Joi.string().min(2).max(100).required()
   })
   return joiSchema.validate(_reqbody)
 }
